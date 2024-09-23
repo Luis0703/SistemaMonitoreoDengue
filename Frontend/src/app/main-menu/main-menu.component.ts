ee9b4344113e52031b1f309cb757f6d7e094a4b0
Luis0703 <luis.casaperalta@unmsm.edu.pe>
2024-09-23 00:10:18 -0500

    main-menu_v1

---
+++ Frontend/src/app/main-menu/main-menu.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+interface ChartData {
+  name: string;
+  casos: number;
+}
+
+@Component({
+  selector: 'app-main-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-menu.component.html',
+  styleUrls: ['./main-menu.component.scss']
+})
+export class MainMenuComponent implements OnInit {
+  chartData: ChartData[] = [
+    { name: 'Ene', casos: 4000 },
+    { name: 'Feb', casos: 3000 },
+    { name: 'Mar', casos: 2000 },
+    { name: 'Abr', casos: 2780 },
+    { name: 'May', casos: 1890 },
+    { name: 'Jun', casos: 2390 },
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
